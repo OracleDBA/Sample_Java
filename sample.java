feafe4eab3801fc2a11294f250a0f7df100ba5f3
rahil <rahil@github.com>
2019-03-18 07:26:50 +0000

    Initial Java Code

---
+++ sample.java
@@ -0,0 +1,9 @@
+class sample
+{
+
+	public static void main(String args[]) {
+
+		System.out.println("Welcome to Jenkins Server Integration and Deployment");	
+		System.out.println("Maintained and Managed by Rahil - Architect");
+	}
+}
